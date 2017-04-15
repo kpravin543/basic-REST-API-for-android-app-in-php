5d1c2ebe6da0061bc91ecaed9eb6c579d74b8e97
Pravin Kumar <kpravin543@gmail.com>
2017-04-16 00:43:58 +0530

    Update userclass.php

--- userclass.php
+++ userclass.php
@@ -1,9 +1,15 @@
+////////user class all define all the functions this class in order to organise your code/////////////
+
 class user {
    public function __construct(){
    }
    
    public function register_user($conn,$fname,$lastname,$mobile,$dob){
-        $query= "INSERT INTO register_user VALUES ('$fname','$lastname','$mobile','$dob')";
+        $query= "INSERT INTO register_table VALUES ('$fname','$lastname','$mobile','$dob')";
         
    }
+
+  public function check_login($conn, $email, $pass){
+         $query = "SELECT *FROM register_table WHERE email='$email' AND password= '$pass'";
+  }
 }
