e7b357c49b1ee24574d9c37adf540493033928ae
Pravin Kumar <kpravin543@gmail.com>
2017-04-16 13:04:44 +0530

    Update userclass.php

--- userclass.php
+++ userclass.php
@@ -5,8 +5,23 @@ class user {
    }
    
    public function register_user($conn,$fname,$lastname,$mobile,$dob){
-        $query= "INSERT INTO register_table VALUES ('$fname','$lastname','$mobile','$dob')";
-        
+        $stmt = $conn->prepare("INSERT INTO register_table (FirstName,LastName,Mobile,DOB) 
+        VALUES ($fname, $lname, $mobile, $DOB)");
+        $stmt->bindParam(':nam', $txtNam);
+        $stmt->bindParam(':add', $txtAdd);
+        $stmt->bindParam(':cit', $txtCit);
+        $stmt->execute();
+       
+      if($num_rows>0){
+          $response['status']=1;
+          $response['message']='success';
+         
+          return $response;
+              }
+         $response['status']=0;
+          $response['message']='failed';
+         
+          return $response;
    }
 
   public function check_login($conn, $email, $pass){
