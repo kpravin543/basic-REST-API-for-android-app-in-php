16324dc7cfe1f0d74f61be9c993fe90aeb978faa
Pravin Kumar <kpravin543@gmail.com>
2017-04-16 23:34:42 +0530

    Update userclass.php

--- userclass.php
+++ userclass.php
@@ -5,22 +5,36 @@ class user {
    }
    
    public function register_user($conn,$fname,$lastname,$mobile,$dob){
-        $stmt = $conn->prepare("INSERT INTO register_table (FirstName,LastName,Mobile,DOB) 
-        VALUES ($fname, $lname, $mobile, $DOB)");
-        $stmt->bindParam(':nam', $txtNam);
-        $stmt->bindParam(':add', $txtAdd);
-        $stmt->bindParam(':cit', $txtCit);
-        $stmt->execute();
-       
+        try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // prepare sql and bind parameters
+    $stmt = $conn->prepare("INSERT INTO register_user ($firstname, $lastname, $mobile, $dob) 
+    VALUES (:firstname, :lastname, :mobile, :dob)");
+    $stmt->bindParam(':firstname', $firstname);
+    $stmt->bindParam(':lastname', $lastname);
+    $stmt->bindParam(':mobile', $mobile);
+    $stmt->bindParam(':dob',$dob);
+
+    $stmt->execute();
+
+    echo "New records created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Error: " . $e->getMessage();
+    }
+$conn = null;
+        
+        $num_rows= $stmt->affected_rows;
+        $stmt->close();
       if($num_rows>0){
           $response['status']=1;
-          $response['message']='success';
-         
           return $response;
               }
          $response['status']=0;
-          $response['message']='failed';
-         
           return $response;
    }
 
